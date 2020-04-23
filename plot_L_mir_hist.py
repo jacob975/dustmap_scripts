c3998e1f31014727456018c93b4eec2f9e8901f3
jacob975 <z123a123s123@gmail.com>
2020-04-23 17:28:54 +0800

    Add functions to extract data from irsa c2d catalog, and also plot L_mir histogram.

--- plot_L_mir_hist.py
+++ plot_L_mir_hist.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''
 Abstract:
-    Plot the luminosity histogram with the given table.
+    Plot the mid-infrared luminosity histogram with the given table.
 Usage:
-    plot_L_hist.py [paras of luminosity func.] [Q labels]
+    plot_L_mir_hist.py [paras of luminosity func.] [Q labels]
 Output:
     A histogram of luminosity.
 Editor:
@@ -14,7 +14,7 @@ Editor:
 #   This code is made in python3 #
 ##################################
 
-20200421 
+20200422
 ####################################
 update log
 20200421 version alpha 1:
@@ -69,15 +69,15 @@ if __name__ == "__main__":
     Q_array = np.loadtxt(Q_name, dtype = str)
     #-----------------------------------------
     # List 1: All sources
-    L_bol_list_1 = paras_array[:, 2]
+    L_mir_list_1 = paras_array[:, 1]
     # List 2: Source without saturation. 
     index_non_saturated = find_non_saturated(Q_array)
-    L_bol_list_2 = paras_array[index_non_saturated, 2]
+    L_mir_list_2 = paras_array[index_non_saturated, 1]
     #-----------------------------------
     # Plot the histogram
     fig, axes = plt.subplots(figsize = (8,6))
     plt.hist(
-        L_bol_list_1,
+        L_mir_list_1,
         bins = np.logspace(-4, 4, num = 17),
         lw = 3,
         alpha = 0.5,
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         label = "All sources"
     )
     plt.hist(
-        L_bol_list_2,
+        L_mir_list_2,
         bins = np.logspace(-4, 4, num = 17),
         lw = 3,
         alpha = 0.5,
@@ -94,8 +94,8 @@ if __name__ == "__main__":
     )
     axes.legend()
     axes.set_xscale("log")
-    axes.set_xlabel(r"$L_{bol} (L_{solar})$")
-    plt.savefig("L_bol_hist.png")
+    axes.set_xlabel(r"$L_{mir} (L_{solar})$")
+    plt.savefig("L_mir_hist.png")
     #-----------------------------------
     # Measure time
     elapsed_time = time.time() - start_time
