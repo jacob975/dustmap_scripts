Add function to calculate the average distance to cloud regions, and some minor fixes.

Av_region_mass.py
@@ -3,7 +3,7 @@
 Abstract:
     This is a program to calculate the cloud mass based on column density map and extintion map.
 Usage:
-    Av_region_mass.py [contour config] [column density map] [extinction map]
+    Av_region_mass.py [contour config] [cloud_name] [column density map] [extinction map]
     column density map unit: M_sun / kpc^2
     extinction map unit: Av in mag
 Output:
@@ -101,14 +101,15 @@ if __name__ == "__main__":
     aa = option_Av_region_paras(default_contour_config_name)
     #-----------------------------------
     # Load argv
-    if len(argv) != 4:
+    if len(argv) != 5:
         print ("The number of arguments is wrong.")
-        print ("Usage:  Av_region_mass.py [contour config] [column density map] [extinction map]") 
+        print ("Usage:  Av_region_mass.py [contour config] [cloud_name] [column density map] [extinction map]") 
         aa.create()
         exit()
     contour_config_name = argv[1]
-    col_den_name = argv[2]
-    Av_name = argv[3]
+    cloud_name = argv[2]
+    col_den_name = argv[3]
+    Av_name = argv[4]
     print("Arguments:\n{0}".format(argv))
     #--------------------------------------------
     # Load image
@@ -139,8 +140,7 @@ if __name__ == "__main__":
     result_table = np.zeros((len(levels), 5), dtype = object)
     #--------------------------------------------
     # Given cloud distance
-    u_distance = dist_lib.serpens_distance
-
+    u_distance = dist_lib.distance_dict[cloud_name]
     # Estimate the cloud mass
     print("distance (pc): {0}".format(u_distance))
     prev_level = None

cloud_mass.py
@@ -3,7 +3,7 @@
 Abstract:
     This is a program to calculate the cloud mass based on column density map and extintion map.
 Usage:
-    cloud_mass.py [column density map] [extinction map]
+    cloud_mass.py [contour config] [cloud name] [column density map] [extinction map]
     column density map unit: M_sun / kpc^2
     extinction map unit: Av in mag
 Output:
@@ -38,6 +38,7 @@ from astropy.wcs.utils import proj_plane_pixel_area
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 
+from input_lib import option_Av_region_paras
 import dist_lib
 
 def calc_cloud_mass(col, e_col, mask, pix_area_deg2, u_distance_pc):
@@ -77,13 +78,21 @@ if __name__ == "__main__":
     # Measure time
     start_time = time.time()
     #-----------------------------------
+    # Initialize the arguments
+    default_contour_config_name = "contour_config.txt"
+    # aa stands for Arguement Assistent
+    aa = option_Av_region_paras(default_contour_config_name)
+    #-----------------------------------
     # Load argv
-    if len(argv) != 3:
+    if len(argv) != 5:
         print ("The number of arguments is wrong.")
-        print ("Usage: cloud_mass.py [column density] [extinction map]" )
+        print ("Usage: cloud_mass.py [contour_config] [cloud name] [column density] [extinction map]" )
+        aa.create()
         exit()
-    col_den_name = argv[1]
-    Av_name = argv[2]
+    contour_config_name = argv[1]
+    cloud_name = argv[2]
+    col_den_name = argv[3]
+    Av_name = argv[4]
     print("Arguments:\n{0}".format(argv))
     #--------------------------------------------
     # Load image
@@ -98,17 +107,22 @@ if __name__ == "__main__":
     w_Av = WCS(h_Av)
     #--------------------------------------------
     # Initialize the parameters
+    # Contours
+    contour_config = aa.load(contour_config_name)
+    levels = np.array(contour_config[0], dtype = float)
+    linewidths = np.array(contour_config[1], dtype = float)
+    colors = contour_config[2]
+    print("Av levels: \n{0}".format(levels))
+    print("linewidth: \n{0}".format(linewidths))
+    print("colors: \n{0}".format(colors))
+    # Pixel Size
     pix_area_in_deg2_col = abs(h_col['CDELT1'] * h_col['CDELT2']) 
     pix_area_in_deg2_Av = abs(h_Av['CDELT1'] * h_Av['CDELT2'])
     #--------------------------------------------
     # Given cloud distance
-    u_distance = dist_lib.serpens_distance
+    u_distance = dist_lib.distance_dict[cloud_name]
     # Estimate the cloud mass
     print("distance (pc): {0}".format(u_distance))
-    levels = [2, 4, 6, 9, 12, 22] 
-    linewidths = [2, 1.5, 1.5, 1.5, 1.5, 1.5]
-    colors = ['k', 'r', 'k', 'k', 'b', 'k']
-    print("Av levels: \n{0}".format(levels))
     for level in levels:
         Av_mask = np.where(Av > level)
         calc_cloud_mass(col, e_col, Av_mask, pix_area_in_deg2_col, u_distance)

input_lib.py
@@ -153,11 +153,11 @@ class option_Av_region_paras(inp_option):
             s = [
                 "# The Av list used to define contours from little to large."
                 "# ex. : 2 4 6 12 22",
-                "2   4   6   9  12  22",
+                "0.1    2   4   6   9  12  22",
                 "# The contour linewidth",
-                "2 1.5 1.5 1.5 1.5 1.5",
+                "2      2 1.5 1.5 1.5 1.5 1.5",
                 "# The contour colors",
-                "k   r   k   k   b   k",
+                "k      k   r   k   k   b   k",
             ]
             np.savetxt(self.default_name, s, fmt = '%s')
             return
@@ -165,4 +165,26 @@ class option_Av_region_paras(inp_option):
             return
     def load(self, file_name):
         self.opts = np.loadtxt(file_name, dtype = str)
+        return self.opts
+
+class option_avg_distance(inp_option):
+    def create(self):
+        # Create the option file if it doesn't exist.
+        try:
+            np.loadtxt(self.default_name, dtype = str)
+        except:
+            s = [
+                "# One row represents the distance of a region.",
+                "# ex. 1064 53",
+                "# You can put more than one region below.",
+                "1064 53",
+                "1046 52",
+            ]
+            np.savetxt(self.default_name, s, fmt = '%s')
+            return
+        else:
+            return
+    def load(self, file_name):
+        self.opts = np.loadtxt(file_name, dtype = float)
+        self.opts = self.opts.reshape((-1, 2))
         return self.opts

plot_sfr_vs_gas.py
@@ -28,6 +28,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from chiu20_mysql_lib import load2py_mq_cloud
+from Heiderman10_lib import Heiderman_cloud, index_HC
 #--------------------------------------------
 # Main code
 if __name__ == "__main__":
@@ -41,64 +42,53 @@ if __name__ == "__main__":
         exit()
     #--------------------------------------------
     # Initialization
-    col_list = [
-        '`cloud_surface_density_Msun_per_pc2`', 
-        '`sfr_surface_density_Msun_per_Myr_pc2`'
+    YSO_col_list = [
+        '`cloud_surface_density_Msun_per_pc2`',
+        '`e_cloud_surface_density_Msun_per_pc2`',
+        '`sfr_surface_density_Msun_per_Myr_pc2`',
+        '`e_sfr_surface_density_Msun_per_Myr_pc2`',
+        '`flag_sfr_surface_density_Msun_per_Myr_pc2`',
     ]
-    col_list_str = '|'.join(col_list)
+    col_list_str = '|'.join(YSO_col_list)
     # Obtain data from SQL
-    data = load2py_mq_cloud(col_list)
-    data = np.array(data, dtype=float)
-    gas_sigma_c2d_gould_belt = data[:20,0]
-    sfr_sigma_c2d_gould_belt = data[:20,1]
-    gas_sigma_others = data[20:52,0]
-    sfr_sigma_others = data[20:52,1]
+    c2d_gould_belt_data = load2py_mq_cloud(YSO_col_list)
+    c2d_gould_belt_data = np.array(c2d_gould_belt_data, dtype=object)
+    gas_sigma_c2d_gould_belt =       np.array(c2d_gould_belt_data[:,0], dtype = float)
+    e_gas_sigma_c2d_gould_belt =     np.array(c2d_gould_belt_data[:,1], dtype = float)
+    sfr_sigma_c2d_gould_belt =       np.array(c2d_gould_belt_data[:,2], dtype = float)
+    e_sfr_sigma_c2d_gould_belt =     np.array(c2d_gould_belt_data[:,3], dtype = float)
+    flag_sfr_sigma_c2d_gould_belt =  np.array(c2d_gould_belt_data[:,4], dtype = str)
+    index_U_c2d_gould_belt = flag_sfr_sigma_c2d_gould_belt == 'U'
     #--------------------------------------------
     # Plot the figure
     fig, ax = plt.subplots(figsize = (8,8))
-    ax.scatter(
-        gas_sigma_c2d_gould_belt, 
-        sfr_sigma_c2d_gould_belt, 
-        label='My works (clouds from c2d, Gould belt)'
-    )
-    ax.scatter(
-        gas_sigma_others, 
-        sfr_sigma_others, 
-        label='My works (clouds from Zucker+20)'
+    ax.errorbar(
+        x = gas_sigma_c2d_gould_belt[~index_U_c2d_gould_belt],
+        xerr = e_gas_sigma_c2d_gould_belt[~index_U_c2d_gould_belt],
+        y = sfr_sigma_c2d_gould_belt[~index_U_c2d_gould_belt],
+        yerr = e_sfr_sigma_c2d_gould_belt[~index_U_c2d_gould_belt],
+        label = 'c2d and Gould belt clouds',
+        color = 'b',
+        fmt = 'o',
     )
     #--------------------------------------------
     # Additional data
     #-------------
     # Heiderman+10
-    Heiderman_cloud = np.array([
-    # Cloud, gas_sigma, sfr_sigma
-        [ 'Cha II  ', 64.3, 0.605],
-        [ 'Lup I   ', 57.9, 0.367],
-        [ 'Lup III ', 59.2, 1.10],
-        [ 'Lup IV  ', 75.0, 1.19],
-        [ 'Oph     ', 105 , 2.45],
-        [ 'Per     ', 90.0, 1.31],
-        [ 'Ser     ', 138 , 3.29],
-        [ 'AurN    ', 92.9, 0.207],
-        [ 'Aur     ', 92.4, 0.854],
-        [ 'Cep     ', 68.7, 0.776],
-        [ 'Cha III ', 47.5, 0.0357],
-        [ 'Cha I   ', 91.1, 2.36],
-        [ 'CrA     ', 92.1, 3.37],
-        [ 'IC5146E ', 54.9, 0.378],
-        [ 'IC5146NW', 59.1, 0.108],
-        [ 'Lup VI  ', 67.5, 1.66],
-        [ 'Lup V   ', 60.3, 0.915],
-        [ 'Mus     ', 49.1, 0.440],
-        [ 'Sco     ', 85.2, 0.343],
-        [ 'Ser-Aqu ', 136 , 2.01],
-    ], dtype = object)
-    Heiderman_gas_sigma = np.array(Heiderman_cloud[:,1], dtype = float)
-    Heiderman_sfr_sigma = np.array(Heiderman_cloud[:,2], dtype = float)
-    ax.scatter(
-        Heiderman_gas_sigma, 
-        Heiderman_sfr_sigma, 
-        label = 'Heiderman+10 (c2d, Gould belt)')
+    #TODO
+    Heiderman_gas_sigma = np.array(Heiderman_cloud[:,index_HC.index('gas_sigma')], dtype = float)
+    e_Heiderman_gas_sigma = np.array(Heiderman_cloud[:,index_HC.index('e_gas_sigma')], dtype = float)
+    Heiderman_sfr_sigma = np.array(Heiderman_cloud[:,index_HC.index('sfr_sigma')], dtype = float)
+    e_Heiderman_sfr_sigma = np.array(Heiderman_cloud[:,index_HC.index('e_sfr_sigma')], dtype = float)
+    ax.errorbar(
+        x = Heiderman_gas_sigma,
+        xerr = e_Heiderman_gas_sigma,
+        y = Heiderman_sfr_sigma, 
+        yerr = e_Heiderman_sfr_sigma,
+        label = 'Heiderman+10 (c2d and Gould belt clouds)',
+        color = 'g',
+        fmt = 'o',
+    )
     #-------------
     # Kennicutt+98
     # K-S relation

plot_sfr_vs_gas_all_YSO.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         xerr = e_gas_sigma_class_I[~index_U_class_I],
         y = sfr_sigma_class_I[~index_U_class_I],
         yerr = e_sfr_sigma_class_I[~index_U_class_I],
-        label='My works (c2d Class I)',
+        label= 'Class I YSO',
         color = 'b',
         fmt = 'o'
     )
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     ax.scatter(
         x = gas_sigma_class_I[index_U_class_I],
         y = sfr_sigma_class_I[index_U_class_I],
-        label='My works (c2d Class I upper limits)', 
+        label='Class I YSO upper limit', 
         marker = 'v',
         color = 'b',
         alpha = 0.5,
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         xerr = e_gas_sigma_class_F[~index_U_class_F],
         y = sfr_sigma_class_F[~index_U_class_F],
         yerr = e_sfr_sigma_class_F[~index_U_class_F],
-        label='My works (c2d Class F)',
+        label='Class Flat YSO',
         color = 'm',
         fmt = 'o'
     )
@@ -113,7 +113,7 @@ if __name__ == "__main__":
     ax.scatter(
         x = gas_sigma_class_F[index_U_class_F],
         y = sfr_sigma_class_F[index_U_class_F],
-        label='My works (c2d Class F upper limits)', 
+        label='Class Flat YSO upper limit', 
         marker = 'v',
         color = 'm',
         alpha = 0.5,
@@ -127,18 +127,18 @@ if __name__ == "__main__":
     Heiderman_sfr_sigma_class_i = np.array(Heiderman_Av_regions_class_i[:,2], dtype = float)
     Heiderman_gas_sigma_class_f = np.array(Heiderman_Av_regions_class_f[:,1], dtype = float)
     Heiderman_sfr_sigma_class_f = np.array(Heiderman_Av_regions_class_f[:,2], dtype = float)
-    ax.scatter(
-        Heiderman_gas_sigma_class_i, 
-        Heiderman_sfr_sigma_class_i, 
-        label = 'Heiderman+10 (c2d class I)', 
-        color = 'g',
-    )
-    ax.scatter(
-        Heiderman_gas_sigma_class_f, 
-        Heiderman_sfr_sigma_class_f, 
-        label = 'Heiderman+10 (c2d class F)', 
-        color = 'c',
-    )
+    #ax.scatter(
+    #    Heiderman_gas_sigma_class_i, 
+    #    Heiderman_sfr_sigma_class_i, 
+    #    label = 'Heiderman+10 (c2d class I)', 
+    #    color = 'g',
+    #)
+    #ax.scatter(
+    #    Heiderman_gas_sigma_class_f, 
+    #    Heiderman_sfr_sigma_class_f, 
+    #    label = 'Heiderman+10 (c2d class F)', 
+    #    color = 'c',
+    #)
     #-------------
     # Kennicutt+98
     # K-S relation