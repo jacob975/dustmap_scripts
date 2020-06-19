ccde6fbbb1e9cdf62d4f9c0f5700ba479d1e1a54
jacob975 <z123a123s123@gmail.com>
2020-06-19 13:43:56 +0800

    Add functions to plot the SFR-gas relation.

---
+++ plot_sfr_vs_gas_all_YSO_500pc_1000pc.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+'''
+Abstract:
+    Plot the SFR-gas relation in different Av contour regions (Without error consideration).
+Usage:
+    plot_sfr_vs_gas.py.
+Output:
+    1. The figure of SFR surface density and gas surface density.
+Editor:
+    Jacob975
+
+##################################
+#   Python3                      #
+#   This code is made in python3 #
+##################################
+
+20200522
+####################################
+update log
+20200522 version alpha 1:
+    1. The code works.
+'''
+# First, we’ll import the necessary modules:
+import time
+from sys import argv
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from chiu20_mysql_lib import load2py_mq_av_region, load2py_mq_cloud
+from Heiderman10_lib import Heiderman_cloud, Heiderman_Av_regions_class_f, Heiderman_Av_regions_class_i
+#--------------------------------------------
+# Main code
+if __name__ == "__main__":
+    # Measure time
+    start_time = time.time()
+    #-----------------------------------
+    # Load argv
+    if len(argv) != 1:
+        print ("The number of arguments is wrong.")
+        print ("Usage: plot_sfr_vs_gas.py")
+        exit()
+    #--------------------------------------------
+    # Initialization
+    class_I_list = [
+        '`cloud_surface_density_Msun_per_pc2`', 
+        '`e_cloud_surface_density_Msun_per_pc2`', 
+        '`sfr_I_surface_density_Msun_per_Myr_pc2`',
+        '`e_sfr_I_surface_density_Msun_per_Myr_pc2`',
+        '`flag_sfr_I_surface_density_Msun_per_Myr_pc2`',
+        '`distance_pc`',
+    ]
+    class_F_list = [
+        '`cloud_surface_density_Msun_per_pc2`', 
+        '`e_cloud_surface_density_Msun_per_pc2`', 
+        '`sfr_F_surface_density_Msun_per_Myr_pc2`',
+        '`e_sfr_F_surface_density_Msun_per_Myr_pc2`',
+        '`flag_sfr_F_surface_density_Msun_per_Myr_pc2`',
+        '`distance_pc`',
+    ]
+    # Obtain data from SQL
+    # Class I, Zucker+20 sources only
+    class_I_data = load2py_mq_av_region(class_I_list)
+    class_I_data = np.array(class_I_data, dtype=object)
+    gas_sigma_class_I =       np.array(class_I_data[:,0], dtype = float)
+    e_gas_sigma_class_I =     np.array(class_I_data[:,1], dtype = float)
+    sfr_sigma_class_I =       np.array(class_I_data[:,2], dtype = float)
+    e_sfr_sigma_class_I =     np.array(class_I_data[:,3], dtype = float)
+    flag_sfr_sigma_class_I =  np.array(class_I_data[:,4], dtype = str)
+    distance_class_I =        np.array(class_I_data[:,5], dtype = float)
+    index_U_class_I = flag_sfr_sigma_class_I == 'U'
+    index_500pc_1000pc_class_I = np.logical_and(distance_class_I > 500, distance_class_I <= 1000)
+    print(index_500pc_1000pc_class_I)
+    # Class Flat, Zucker+20 sources only
+    class_F_data = load2py_mq_av_region(class_F_list)
+    class_F_data = np.array(class_F_data, dtype=object)
+    gas_sigma_class_F =      np.array(class_F_data[:,0], dtype = float)
+    e_gas_sigma_class_F =    np.array(class_F_data[:,1], dtype = float)
+    sfr_sigma_class_F =      np.array(class_F_data[:,2], dtype = float)
+    e_sfr_sigma_class_F =    np.array(class_F_data[:,3], dtype = float)
+    flag_sfr_sigma_class_F = np.array(class_F_data[:,4], dtype = str)
+    distance_class_F =        np.array(class_F_data[:,5], dtype = float)
+    index_U_class_F = flag_sfr_sigma_class_F == 'U'
+    index_500pc_1000pc_class_F = np.logical_and(distance_class_F > 500, distance_class_F <= 1000)
+    #--------------------------------------------
+    # Plot the figure
+    fig, ax = plt.subplots(figsize = (8,8))
+    #----------
+    # Class_I 
+    if len(gas_sigma_class_I[(~index_U_class_I) & (index_500pc_1000pc_class_I)]) != 0:
+        ax.errorbar(
+            x = gas_sigma_class_I[(~index_U_class_I) & (index_500pc_1000pc_class_I)],
+            xerr = e_gas_sigma_class_I[(~index_U_class_I) & (index_500pc_1000pc_class_I)],
+            y = sfr_sigma_class_I[(~index_U_class_I) & (index_500pc_1000pc_class_I)],
+            yerr = e_sfr_sigma_class_I[(~index_U_class_I) & (index_500pc_1000pc_class_I)],
+            label= 'Class I YSO',
+            color = 'b',
+            fmt = 'o'
+        )
+    # SFR Upper limits for Av regions without a YSO.
+    if len(gas_sigma_class_I[(index_U_class_I) & (index_500pc_1000pc_class_I)]) != 0:
+        ax.scatter(
+            x = gas_sigma_class_I[(index_U_class_I) & (index_500pc_1000pc_class_I)],
+            y = sfr_sigma_class_I[(index_U_class_I) & (index_500pc_1000pc_class_I)],
+            label='Class I YSO upper limit', 
+            marker = 'v',
+            color = 'b',
+            alpha = 0.5,
+        )
+    #----------
+    # Class_F
+    if len(gas_sigma_class_F[(~index_U_class_F) & (index_500pc_1000pc_class_F)]) != 0:
+        ax.errorbar(
+            x = gas_sigma_class_F[(~index_U_class_F) & (index_500pc_1000pc_class_F)],
+            xerr = e_gas_sigma_class_F[(~index_U_class_F) & index_500pc_1000pc_class_F],
+            y = sfr_sigma_class_F[(~index_U_class_F) & (index_500pc_1000pc_class_F)],
+            yerr = e_sfr_sigma_class_F[(~index_U_class_F) & (index_500pc_1000pc_class_F)],
+            label='Class Flat YSO',
+            color = 'm',
+            fmt = 'o'
+        )
+    # SFR Upper limits for Av regions without a YSO.
+    if len(gas_sigma_class_F[(index_U_class_F) & (index_500pc_1000pc_class_F)]) != 0:
+        ax.scatter(
+            x = gas_sigma_class_F[(index_U_class_F) & (index_500pc_1000pc_class_F)],
+            y = sfr_sigma_class_F[(index_U_class_F) & (index_500pc_1000pc_class_F)],
+            label='Class Flat YSO upper limit', 
+            marker = 'v',
+            color = 'm',
+            alpha = 0.5,
+        )
+    #--------------------------------------------
+    # Additional data
+    #-------------
+    # Heiderman+10
+    Heiderman_gas_sigma_class_i = np.array(Heiderman_Av_regions_class_i[:,1], dtype = float)
+    Heiderman_sfr_sigma_class_i = np.array(Heiderman_Av_regions_class_i[:,2], dtype = float)
+    Heiderman_gas_sigma_class_f = np.array(Heiderman_Av_regions_class_f[:,1], dtype = float)
+    Heiderman_sfr_sigma_class_f = np.array(Heiderman_Av_regions_class_f[:,2], dtype = float)
+    #ax.scatter(
+    #    Heiderman_gas_sigma_class_i, 
+    #    Heiderman_sfr_sigma_class_i, 
+    #    label = 'Heiderman+10 (c2d class I)', 
+    #    color = 'g',
+    #)
+    #ax.scatter(
+    #    Heiderman_gas_sigma_class_f, 
+    #    Heiderman_sfr_sigma_class_f, 
+    #    label = 'Heiderman+10 (c2d class F)', 
+    #    color = 'c',
+    #)
+    #-------------
+    # Kennicutt+98
+    # K-S relation
+    def Kennicut98_sfr_sigma(gas_sigma):
+        # gas_sigma in Msun / pc^2
+        # sfr_sigma in Msun / Myr pc^2
+        sfr_sigma = 2.5e-4 * np.power(gas_sigma, 1.4)
+        return sfr_sigma
+    KS_gas_sigma = np.logspace(1, 3, 100)
+    KS_sfr_sigma = Kennicut98_sfr_sigma(KS_gas_sigma)
+    ax.plot(
+        KS_gas_sigma, 
+        KS_sfr_sigma, 
+        color = 'k', 
+        label = 'Kennicut+98 ( KS relation)'
+    )
+    #-----------------------------------
+    # Adjust and Save the figure
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    #ax.set_xlim(1e0, 1e3)
+    #ax.set_ylim(1e-2, 1e1)
+    ax.grid(True)
+    ax.set_xlabel(r'gas surface density ($M_{sun} / pc^{2}$)')
+    ax.set_ylabel(r'SFR surface density ($M_{sun} / Myr pc^{2}$)')
+    ax.legend()
+    fig.savefig("chiu20_sfr_vs_gas_Av_regions_500pc_1000pc.png")
+    #-----------------------------------
+    # Measure time
+    elapsed_time = time.time() - start_time
+    print("Exiting Main Program, spending ", elapsed_time, "seconds.")
